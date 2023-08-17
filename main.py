536c9a21ebffbb8edc5f5e1a0c22ae60443f839a
jkitty843 <69548915+jkitty843@users.noreply.github.com>
2023-08-17 19:18:12 +0000

    Import libraries, zeta function

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
+import matplotlib.pyplot as plt
+#Library contains zeta(), zetazero()
+from mpmath import *
+
+mp.dps = 25
+
+#Attempted self-implementation
+'''
 def rzf(s):
   y = 0
   for d in range(1000):
     y += (1 / pow((d + 1), s))
   return y
-
+'''
 
 while True:
   print('Enter a value for the RZF: \n')
   s = float(input())
-  print(f'Output: {str(rzf(s))}')
+  print(f'Output: {str(zeta(s))}')
